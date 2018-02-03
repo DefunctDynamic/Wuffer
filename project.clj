5f07d3f3ae9f4536289e09f5026d8ce107fe42e9
Ranjeet Singh <ranjeet@raeoks.com>
2018-02-03 19:32:38 +0530

    Add some d-_-b music

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot wuffer.main
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[mud "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clojure/tools.nrepl "0.2.12"]
